5b484190a38cbdef7b2f1b0cf508504a70859cea
Neil Hanson <pro.neilhanson@gmail.com>
2022-06-11 21:30:16 -0500

    updates

--- src/components/GalleryList/GalleryListItem.jsx
+++ src/components/GalleryList/GalleryListItem.jsx
@@ -3,9 +3,9 @@ import './GalleryListItem.css'
 function GalleryListItem({frogItem}) {
     return (
         <>
-            <div key={frogItem.id}>
-                <p>{frogItem.id}</p>
+            <div key={frogItem.id} className="frog_box">
                 <img className="frog_pic" src={frogItem.path}/>
+                <p>{frogItem.description}</p>
             </div>
         </>
     );
