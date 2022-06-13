770ffc6b59b1bdc69faaa416d661071cd806b526
Neil Hanson <pro.neilhanson@gmail.com>
2022-06-13 08:45:41 -0500

    more notes

--- src/components/GalleryList/GalleryListItem.jsx
+++ src/components/GalleryList/GalleryListItem.jsx
@@ -1,11 +1,17 @@
+// Importing useState because we need to keep track of the
+//  changes that are made when we click on a frog element. 
 import { useState } from 'react';
 import './GalleryListItem.css'
 
-
+// Here is the component with the two props we sent over from 'GalleryList.jsx'.
+// Again, we are using {}'s to destructure our props to simplify them without typing their prefixes.
 function GalleryListItem({frogItem, addLike}) {
 
+    // Here we are using useState to keep track of the true/false status that we are giving
+    //  our rendered element below. It is going to be a toggle effect, so we're using a boolean here.
     const [status, setStatus] = useState(false);
 
+    
     const likeBtn = () => {
         console.log(`--- In addLike ---`, frogItem.id);
         addLike(frogItem.id);
